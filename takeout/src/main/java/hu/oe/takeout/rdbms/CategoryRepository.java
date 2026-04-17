94341bf3adc1a9178e3323c1471e3cab1ea44f0d
Akkornincsnevem@stud.uni-obuda.hu <akkornincsnevem@stud.uni-obuda.hu>
2026-04-17 10:29:26 +0200

    added Repository annotation moved Entity to be in Spring boot scan path

--- takeout/src/main/java/hu/oe/takeout/rdbms/CategoryRepository.java
+++ takeout/src/main/java/hu/oe/takeout/rdbms/CategoryRepository.java
@@ -1,10 +1,11 @@
 package hu.oe.takeout.rdbms;
 
-import hu.oe.yokudlela.takeout.generated.entity.Category;
+import hu.oe.takeout.entity.Category;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface CategoryRepository extends CrudRepository<Category, UUID> {
 
     boolean existsByName(String pName);
--- takeout/src/main/java/hu/oe/takeout/rdbms/TakeoutRepository.java
+++ takeout/src/main/java/hu/oe/takeout/rdbms/TakeoutRepository.java
@@ -1,10 +1,11 @@
 package hu.oe.takeout.rdbms;
 
-import hu.oe.yokudlela.takeout.generated.entity.Takeout;
+import hu.oe.takeout.entity.Takeout;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface TakeoutRepository extends CrudRepository<Takeout, UUID> {
 
     boolean existsByName(String pName);
